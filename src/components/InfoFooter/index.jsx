fcd0cfee7329b8844aa6fef03e866f2857faa51c
Diego Fernando Dominguez <diegofernandodominguez@gmail.com>
2023-02-13 00:33:14 -0500

    Update footer

--- src/components/InfoFooter/index.jsx
+++ src/components/InfoFooter/index.jsx
@@ -3,7 +3,8 @@ import React from 'react';
 const InfoFooter = () => (
   <footer className="info">
     <p>Double-click to edit a todo</p>
-    <p>Created by <a href="http://todomvc.com">you</a></p>
+    <p>Template by <a href="http://sindresorhus.com">Sindre Sorhus</a></p>
+    <p>Created by <a href="https://github.com/diego4serve">Diego Fernando Dominguez</a></p>
     <p>Part of <a href="http://todomvc.com">TodoMVC</a></p>
   </footer>
 );
